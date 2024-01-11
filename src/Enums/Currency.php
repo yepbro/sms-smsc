3454b83a58c5d3f989e6b0c50dfe3f232ac83a31
Denis <denis@yepbro.ru>
2024-01-11 21:55:28 +0300

    Добавлена поддержка всех возможных для баланса валют

--- src/Enums/Currency.php
+++ src/Enums/Currency.php
@@ -5,4 +5,16 @@ namespace Yepbro\Smsc\Enums;
 enum Currency: string
 {
     case RUB = 'RUB';
+    case USD = 'USD';
+    case EUR = 'EUR';
+    case UAH = 'UAH';
+    case KZT = 'KZT';
+    case GBP = 'GBP';
+    case AZN = 'AZN';
+    case AMD = 'AMD';
+    case KGS = 'KGS';
+    case TJS = 'TJS';
+    case UZS = 'UZS';
+    case MDL = 'MDL';
+    case BYN = 'BYN';
 }
